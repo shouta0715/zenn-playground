528e190e37f574d8e4f8af5021452461d2e35fb9
shouta0715 <Myheroes0715@gmail.com>
2024-02-13 14:42:05 +0900

    add: comment

--- src/components/like-button/use-like-button.ts
+++ src/components/like-button/use-like-button.ts
@@ -11,15 +11,13 @@ type FetchLikeProps = {
   trigger: "like" | "unlike";
 };
 
-export async function fetchLike({ trigger, ...props }: FetchLikeProps) {
+export async function fetchLike({ trigger, ...body }: FetchLikeProps) {
   const result = await fetch("/api/likes", {
     method: trigger === "like" ? "POST" : "DELETE",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({
-      ...props,
-    }),
+    body: JSON.stringify(body),
   });
 
   if (!result.ok) throw new Error("エラーが発生しました。");
@@ -28,37 +26,45 @@ export async function fetchLike({ trigger, ...props }: FetchLikeProps) {
 }
 
 export function useLikeButton({ initialLiked }: UseLikeButtonProps) {
+  // UIに表示する状態
   const [currentLikeState, setCurrentLikeState] = useState(initialLiked);
 
+  // レンダリングには表示しないが、実際の状態を保持する
   const realityLiked = useRef(initialLiked);
 
   const { isPending, mutateAsync } = useMutation({
     mutationFn: fetchLike,
     onSuccess: ({ liked }) => {
+      // 実際の状態に強制的に変更する
       realityLiked.current = liked;
       setCurrentLikeState(liked);
     },
     onError: () => {
-      realityLiked.current = currentLikeState;
+      // エラーが発生した場合は元の状態に戻す
       setCurrentLikeState(realityLiked.current);
     },
   });
 
+  // 連打対策にdebounceを使用する
   const onDebounceLike = useDebouncedCallback(async () => {
+    // 送信中であれば何もしない
     if (isPending) return;
+
+    // 現在の状態と実際の状態が同じであれば何もしない
     if (realityLiked.current === currentLikeState) return;
 
     const trigger = currentLikeState ? "like" : "unlike";
 
-    await mutateAsync({
-      postId: 1,
-      trigger,
-    });
+    await mutateAsync({ postId: 1, trigger });
+
+    // その他の処理...
   }, 500);
 
   const handleLike = () => {
+    // UIの状態を変更する
     setCurrentLikeState((prev) => !prev);
 
+    // 設定したms後までに再度呼び出されなければ実行される
     onDebounceLike();
   };
 
